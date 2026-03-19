769cdf02a09433700c16ea7159cb62820cf9f06e
bellamer <angaben@pm.me>
2026-03-19 18:44:54 +0100

    add profile page, add favicon, disabled upload profile pic button temporarily

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .services import DashboardService
+from .forms import ProfileUpdateForm
+from .models import User
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
@@ -14,3 +18,16 @@ class DashboardView(LoginRequiredMixin, TemplateView):
         context.update(service.get_dashboard_context())
         return context
 
+
+class ProfileView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = ProfileUpdateForm
+    template_name = 'users/profile.html'
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self):
+        return self.request.user
+
+    def form_valid(self, form):
+        messages.success(self.request, 'Your profile has been updated successfully.')
+        return super().form_valid(form)
