fc0acfc3f1244b5e23268df6b64c0379bc772001
Tetiana Ursulova <beskryla.paradie@gmail.com>
2026-04-07 11:08:46 +0200

    add constants.py

---
+++ sprachlernen/constants.py
@@ -0,0 +1,10 @@
+# Shared constants for the Sprach-Lernen app
+
+# Number of correct answers required to mark a word as learned
+LEARNED_THRESHOLD = 5
+
+# Days before a completed list can be reviewed again
+LOCK_DAYS = 7
+
+# SVG circle circumference for progress indicators
+PROGRESS_CIRCLE_CIRCUMFERENCE = 283.0
